b6790a2a52b91020c783163b84d5e359b439db65
mai-pc <sasakimai@MacBook-Pro.local>
2021-01-24 00:42:03 +0900

    complete LP

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -1,22 +1,26 @@
 <template>
   <v-container class="pa-0" fluid>
     <div class="light-blue lighten-2" align="center">
-      <v-row justify="center">
+      <div justify="center" style="position: relative;">
         <h1
-          class="font-weight-black text-center pt-10 brown--text text--darken-4"
+          class="font-weight-black pt-10 brown--text text--darken-4"
           style="font-size: 40px"
         >
           Welcome to this application!
         </h1>
-      </v-row>
-      <v-btn class="ma-5" elevation="2">Login</v-btn>
-      <v-img
-        :src="require('@/assets/image/LpTopImage1.png')"
-        :aspect-ratio="16 / 9"
-        width="500"
-        class="pa-8"
+        <h3 class="pt-10">
+          Twitterで見つけた自分のお気に入りのツイートをまとめてコレクションしよう！
+        </h3>
+        <v-img
+          :src="require('@/assets/image/LpTopImage2.png')"
+          :aspect-ratio="1/1"
+          width="200"
+          style="position: absolute; top: 12px; right: 30px"
+        ></v-img>
+      </div>
+      <v-btn @click="$router.push('top')" class="ma-5" elevation="2"
+        >Login</v-btn
       >
-      </v-img>
       <v-img
         :src="require('@/assets/image/LpTopImage1.png')"
         :aspect-ratio="16 / 9"
@@ -25,15 +29,7 @@
       >
       </v-img>
     </div>
-    <div class="pa-4" style="background-color: #4e454a">
-      <p
-        class="brown--text text--lighten-5 text-center mb-0 pa-4"
-        style="font-size: 20px"
-      >
-        With this application, you will be able to see only the tweets that you
-        want to see！
-      </p>
-    </div>
+    <div style="background-color: #4e454a; height: 80px"></div>
     <div align="center" style="background-color: #f8f5e9">
       <h2
         class="font-weight-bold pa-7 brown--text text--darken-4"
@@ -42,16 +38,15 @@
         works
       </h2>
       <v-row justify="center">
-        <v-col
-          cols="6"
-          v-for="(work, index) in works"
-          :key="index"
-          class="d-flex flex-column"
-        >
-          <v-avatar size="250" class="white mx-auto">
-            <v-img :src="work.img" :aspect-ratio="1 / 1"></v-img>
-          </v-avatar>
-          <p style="font-size: 20px">
+        <v-col cols="6" v-for="(work, index) in works" :key="index">
+          <v-img
+            :src="work.img"
+            :aspect-ratio="1 / 1"
+            width="250"
+            class="mx-auto"
+            style="background-color: white; border-radius: 50%"
+          ></v-img>
+          <p class="my-4" style="font-size: 20px">
             {{ work.detail }}
           </p>
         </v-col>
